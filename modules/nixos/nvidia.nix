ee4fbe2703f866a0e1edc8963e477aea42e5e947
deathvenom54 <devanshkandpal54@gmail.com>
2024-06-27 10:38:40 +0530

    temp

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -1,16 +1,8 @@
-{ config, pkgs, ... }:
+ # https://github.com/NixOS/nixos-hardware/blob/master/common/gpu/nvidia/default.nix
+{ lib, pkgs, ... }:
 
 {
-  # https://github.com/NixOS/nixos-hardware/blob/master/common/gpu/24.05-compat.nix
-  # Backward-compat for 24.05, can be removed after we drop 24.05 support
-  imports = lib.optionals (lib.versionOlder lib.version "24.11pre") [
-    (lib.mkAliasOptionModule [ "hardware" "graphics" "enable" ] [ "hardware" "opengl" "enable" ])
-    (lib.mkAliasOptionModule [ "hardware" "graphics" "extraPackages" ] [ "hardware" "opengl" "extraPackages" ])
-    (lib.mkAliasOptionModule [ "hardware" "graphics" "extraPackages32" ] [ "hardware" "opengl" "extraPackages32" ])
-    (lib.mkAliasOptionModule [ "hardware" "graphics" "enable32Bit" ] [ "hardware" "opengl" "driSupport32Bit" ])
-  ];
-
-  # https://github.com/NixOS/nixos-hardware/blob/master/common/gpu/nvidia/default.nix
+  imports = [ ./24.05-compat.nix ];
   services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
   hardware.graphics.extraPackages = with pkgs; [
     vaapiVdpau
