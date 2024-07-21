9e229fd2fe11a49cc3c47c3c8066d76cebe4d5dd
0xDevansh <devanshkandpal54@gmail.com>
2024-07-21 07:04:49 +0000

    WSL setup for laptop

--- flake.nix
+++ flake.nix
@@ -1,23 +1,37 @@
 {
-  description = "Nixos config flake";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
 
-     home-manager = {
-       url = "github:nix-community/home-manager";
-       inputs.nixpkgs.follows = "nixpkgs";
-     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/default/configuration.nix
-         inputs.home-manager.nixosModules.default
-      ];
+  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs: {
+    nixosConfigurations = {
+      wsl = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
+        modules = [
+          nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "24.05";
+            wsl.enable = true;
+          }
+          ./hosts/wsl/configuration.nix
+           inputs.home-manager.nixosModules.default
+        ];
+      };
+
+      pc = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/default/configuration.nix
+           inputs.home-manager.nixosModules.default
+        ];
+      };
     };
   };
 }
-
